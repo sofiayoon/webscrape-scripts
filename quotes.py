c2b65b1c1df277c9b2c67b074cda3b70b7f54d2d
sofiayoon <60379333+sofiayoon@users.noreply.github.com>
2020-09-08 23:39:18 -0400

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -19,15 +19,5 @@ try:
             quote = parent.find_previous_sibling('span').text
             print(quote)
         print("\n")
-    # print(subscribers)
-    # parent = results.find_parent('span')
-    # print(parent.find_previous_sibling('span').text)
-    # counter = 0
-    # print(title.text)
-    # references = soup.find(
-    #     "ol", {"class": "references"})
-    # for i in references.find_all('li'):
-    #     counter += 1
-    # print("Total of ", counter, " references used")
 except:
     print('Wiki for not found...')
